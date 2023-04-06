0d470ab31204e10dcb58468abc88703239724b92
Yahor Levanenka <witaway33@gmail.com>
2023-04-06 23:54:00 +0300

    Set up Prisma

---
+++ src/database/index.ts
@@ -0,0 +1,13 @@
+// Described in best practices page
+// https://www.prisma.io/docs/guides/other/troubleshooting-orm/help-articles/nextjs-prisma-client-dev-practices
+import { PrismaClient } from '@prisma/client';
+
+const globalForPrisma = global as unknown as {
+	prisma: PrismaClient | undefined;
+};
+
+export const prisma =
+	globalForPrisma.prisma ??
+	new PrismaClient({
+		log: ['query'],
+	});
