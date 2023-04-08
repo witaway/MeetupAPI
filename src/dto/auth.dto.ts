e832878c65dec204326a44e235e4cba07b5599b0
Yahor Levanenka <witaway33@gmail.com>
2023-04-08 22:12:56 +0300

    First DTO is done! HERE'S IMPORTANT NOTE:
    I spent A LOT of time to make work type inferring in controllers. The nuance was in mechanism how TypeScript infers type.
    
    In all other DTOs it's important to make schemas with operators "as const satisfies RequestSchema".
    
    Otherwise, TypeScript will infer more generic type RequestSchema that contain NO INFORMATION about our exact schema, so we won't get correct types and IDE hints.

---
+++ src/dto/auth.dto.ts
@@ -0,0 +1,26 @@
+import { RequestSchema } from '@customTypes/request-schema';
+import { z } from 'zod';
+
+const loginCredentials = z.object({
+	email: z.string().email(),
+	password: z.string().min(8),
+});
+
+const registerCredentials = z.object({
+	email: z.string().email(),
+	name: z.string().min(1),
+	password: z.string().min(8),
+});
+
+export type LoginCredentials = z.infer<typeof loginCredentials>;
+export type RegisterCredentials = z.infer<typeof registerCredentials>;
+
+export const loginSchema: RequestSchema = {
+	body: loginCredentials,
+} as const satisfies RequestSchema;
+
+export const registerSchema = {
+	body: registerCredentials,
+} as const satisfies RequestSchema;
+
+export const logoutSchema = {} as const satisfies RequestSchema;
