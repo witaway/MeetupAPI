d47a1b062ce79c190fb36265383698bd2b8534f5
Yahor Levanenka <witaway33@gmail.com>
2023-04-09 01:35:03 +0300

    Enabled non-null assertion cause in controller sometimes we hardly know whether req.user is undefined or not

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,6 +15,7 @@ module.exports = {
 		jest: true,
 	},
 	rules: {
+		'@typescript-eslint/no-non-null-assertion': 'off',
 		'@typescript-eslint/ban-ts-comment': 'off',
 		'@typescript-eslint/no-empty-interface': 'off',
 		'@typescript-eslint/no-empty-function': 'off',
