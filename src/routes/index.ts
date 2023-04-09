258eb2af584abd349347527a181e7e601492df3f
Yahor Levanenka <witaway33@gmail.com>
2023-04-09 15:13:04 +0300

    fixup! Basic meetup logic

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,11 +3,13 @@ import authRouter from './auth.route';
 import errorsHandler from '@middlewares/errors-handler.middleware';
 import authenticate from '@middlewares/authenticate.middleware';
 import notFoundHandler from '@middlewares/not-found-handler.middleware';
+import meetupRouter from './meetup.route';
 
 const router = Router();
 
 router.use('/auth', authRouter);
 router.use(authenticate);
+router.use('/meetups', meetupRouter);
 
 router.use('*', notFoundHandler);
 router.use(errorsHandler);
