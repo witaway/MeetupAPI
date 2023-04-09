cd40c0ff2b8d86a074af40256cbe8209eb8768c0
Yahor Levanenka <witaway33@gmail.com>
2023-04-09 15:09:33 +0300

    Set default empty tags array and fixed validating data

--- src/dto/meetup.dto.ts
+++ src/dto/meetup.dto.ts
@@ -5,10 +5,7 @@ const meetupInfo = z.object({
 	theme: z.string().min(5).max(50),
 	description: z.string().min(5).max(255),
 	place: z.string().min(5).max(255),
-	time: z.preprocess(
-		(val) => (typeof val === 'number' ? new Date(val) : undefined),
-		z.date(),
-	),
+	time: z.coerce.date(),
 });
 
 export type MeetupInfo = z.infer<typeof meetupInfo>;
@@ -16,7 +13,7 @@ export type MeetupInfo = z.infer<typeof meetupInfo>;
 export const createMeetupSchema = {
 	body: meetupInfo
 		.extend({
-			tags: z.number().array().max(255),
+			tags: z.number().array().max(255).optional().default([]),
 		})
 		.strict(),
 } as const satisfies RequestSchema;
