f2e5f3a24d25b04cbdc77cf59763fefc6e4943a9
Yahor Levanenka <witaway33@gmail.com>
2023-05-05 03:13:39 +0300

    Added hot reloading workaround. Read the docs on link in the file.

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,6 +1,7 @@
 // Described in best practices page
 // https://www.prisma.io/docs/guides/other/troubleshooting-orm/help-articles/nextjs-prisma-client-dev-practices
 import { PrismaClient } from '@prisma/client';
+import * as process from 'process';
 
 const globalForPrisma = global as unknown as {
 	prisma: PrismaClient | undefined;
@@ -11,5 +12,7 @@ const prisma =
 	new PrismaClient({
 		log: ['query'],
 	});
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
 
 export default prisma;
